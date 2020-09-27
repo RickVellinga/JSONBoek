490f4745d06e0262be1856aaf320c79511f2ab67
RickVellinga <29215@ma-web.nl>
2020-09-27 16:47:45 +0200

    Auteurs toegevoegd

--- js/boeken.js
+++ js/boeken.js
@@ -16,12 +16,30 @@ const boeken = {
     uitvoeren() {
       let html = "";
       this.data.forEach(boek => {
-        let titel = "";
+          let completeTitel = "";
         if(boek.voortitel) {
-          titel += boek.voortitel + " ";
+          completeTitel += boek.voortitel + " ";
         }
-        titel += boek.titel;
-        html += `<h3>${titel}</h3>`;
+        completeTitel += boek.titel;
+
+        let auteurs = "";
+        boek.auteurs.forEach(schrijver => {
+            let tv = schrijver.tussenvoegsel ? schrijver.tussenvoegsel +" " : "";
+            let seperator = " ";
+            auteurs += schrijver.voornaam + " " + tv + schrijver.achternaam + seperator;
+        })
+
+        html += `<section class="boek"> `;
+        html += `<img class="boek__cover" src="${boek.cover}" alt="${completeTitel}">`;
+        html += `<h3 class="boek__kopje">${completeTitel}</h3>`;
+        html += `<p class="boek__auteurs">${auteurs}</p>`
+        html += `<span class="boek__uitgave"> ${boek.uitgave}</span>`;
+        html += `<span class="boek__ean"> Ean: ${boek.ean}</span>`;
+        html += `<span class="boek__paginas"> ${boek.paginas} pagina's </span>`;
+        html += `<span class="boek__taal"> ${boek.taal}</span>`;
+        html += `<div class="boek__prijs">&euro; ${boek.prijs}</div>`;
+        html += `</section> `;
+
       });
       uitvoer.innerHTML = html;
     }
